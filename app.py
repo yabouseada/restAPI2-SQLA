71f58f2483ad7d50bf03c137645e404b4d8b1eba
yabouseada <y.abouseada@gmail.com>
2021-09-25 00:40:42 +0200

    v 3

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 from resources.user import UserRegister, UserReview
 from resources.item import Item,ItemList
+from resources.store import Store,StoreList
 from db import db
 
 app = Flask(__name__)
@@ -15,10 +16,15 @@ app.secret_key = 'yahia'
 api= Api(app)
 db.init_app(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 jwt= JWT(app,authenticate,identity) #/auth
 
 
-
+api.add_resource(Store,'/store/<string:name>')
+api.add_resource(StoreList,'/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList,'/items')
 api.add_resource(UserRegister, '/register')
