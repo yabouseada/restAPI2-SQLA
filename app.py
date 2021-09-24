3aa1df024d272f91ed8506e851b36d0da5b0787d
yabouseada <y.abouseada@gmail.com>
2021-09-24 17:41:49 +0200

    v 0.1

---
+++ app.py
@@ -0,0 +1,22 @@
+
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item,ItemList
+
+app = Flask(__name__)
+app.secret_key = 'yahia'
+api= Api(app)
+
+jwt= JWT(app,authenticate,identity) #/auth
+
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList,'/items')
+api.add_resource(UserRegister, '/register')
+if __name__ == '__main__':
+    app.run(port=5000,debug = True)
